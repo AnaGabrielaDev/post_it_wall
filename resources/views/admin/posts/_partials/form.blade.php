5f2863903fac366dc4628ef8d41576bbfc05a965
Ana Gabriela <anagabrieladev@gmail.com>
2022-01-10 12:27:37 -0300

    refactor  🔨: reuse code in the views

---
+++ resources/views/admin/posts/_partials/form.blade.php
@@ -0,0 +1,15 @@
+
+@if ($errors->any())
+<ul>
+   @foreach ($errors->all() as $error)
+    <li>
+        {{$error}}
+    </li>
+   @endforeach
+</ul>
+@endif
+
+@csrf
+<input type="text" name="title" id="title" placeholder="Titulo" value="{{ $posts->title ??old('title')}}">
+<textarea name="content" id="content" cols="30" rows="4" placeholder="Conteudo">{{$posts->content ?? old('content')}}</textarea>
+<button type="submit">Enviar</button>
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -3,20 +3,7 @@
 <h1><a href="{{Route('post.index')}}">Posts</a></h1>
 <h1>Editar o post {{ $posts->title }} </h1>
 
-@if ($errors->any())
-    <ul>
-       @foreach ($errors->all() as $error)
-        <li>
-            {{$error}}
-        </li>
-       @endforeach
-    </ul>
-@endif
-
-<form action=" {{ route('post.update', $posts->id) }} " method="post">
-    @csrf
+<form>
     @method('put')
-    <input type="text" name="title" id="title" placeholder="Titulo" value="{{ $posts->title }}">
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteudo">{{ $posts->content }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.posts._partials.form')
 </form>
