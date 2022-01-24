ebb0309cd40db648ed2872ba844a35cb4152e79f
Ana Gabriela <anagabrieladev@gmail.com>
2022-01-24 15:48:12 -0300

    feat: restrict some routes

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+    Route::post('/post', [PostController::class, 'store'])->name('post.store');
+    Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
+    Route::delete('/post/{id}', [PostController::class, 'destroy'])->name('post.destroy');
+    Route::get('/post/edit/{id}', [PostController::class, 'edit'])->name('post.edit');
+    Route::put('/post/{id}', [PostController::class, 'update'])->name('post.update');
+    Route::any('/post/search', [PostController::class, 'search'])->name('post.search');
+});
+
 Route::get('/post', [PostController::class, 'index'])->name('post.index');
-Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
-Route::post('/post', [PostController::class, 'store'])->name('post.store');
-Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
-Route::delete('/post/{id}', [PostController::class, 'destroy'])->name('post.destroy');
-Route::get('/post/edit/{id}', [PostController::class, 'edit'])->name('post.edit');
-Route::put('/post/{id}', [PostController::class, 'update'])->name('post.update');
-Route::any('/post/search', [PostController::class, 'search'])->name('post.search');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
